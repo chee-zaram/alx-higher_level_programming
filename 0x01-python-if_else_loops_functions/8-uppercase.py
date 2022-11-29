a2da0fbbd33f8ecf31fb350b6934e805c52e0efd
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-11-29 19:46:39 +0100

    Fix bug

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Function to print a string in uppercase
+# Function to display a string in uppercase
 def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
