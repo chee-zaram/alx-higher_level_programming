b998994fdc5ca163eb08686496fc887dac1e212d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-01 13:46:07 +0100

    Add solution to Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     argc = len(argv)
     if argc == 1:
         print("0 arguments.")
-    elif argc > 1:
-        print("{:d} arguments:".format(argc - 1))
+    else:
+        if argc == 2:
+            header = "{:d} argument:".format(argc - 1)
+        else:
+            header = "{:d} arguments:".format(argc - 1)
+        print(header)
         for i in range(1, argc):
             print("{:d}: {}".format(i, argv[i]))
