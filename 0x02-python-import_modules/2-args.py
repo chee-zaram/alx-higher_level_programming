1bcc5f3be23c962f86c17c7c5c5dcc752629c94c
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-01 13:29:05 +0100

    Add solution to Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Program to display the command line arguments passed to the program
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv)
+    if argc == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(argc - 1))
+        for i in range(1, argc):
+            print("{:d}: {}".format(i, argv[i]))
