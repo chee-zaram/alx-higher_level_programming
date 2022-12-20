ea82e0d5d3fa2e3e69193429853b81897391fc60
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-20 17:52:20 +0100

    fix(`my_print`): make sure length of tuple is two
    I changed the comparison from `len > 2` to `len != 2`

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,7 +53,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) > 2:
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != tuple or type(value[1]) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
