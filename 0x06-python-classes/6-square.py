547004200d328bd85cc2a964816576f9fb6ddb89
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-21 00:06:42 +0100

    refactor(class square): remove redundant code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,11 +20,9 @@ class Square:
 
         self.__size = size
 
-        if type(position) != tuple or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) != int or type(position[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
+        if type(position) != tuple or len(position) != 2 \
+            or type(position[0]) != int or type(position[1]) != int \
+                or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = position
@@ -55,11 +53,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) != int or type(value[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        if type(value) != tuple or len(value) != 2 \
+            or type(value[0]) != int or type(value[1]) != int \
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
